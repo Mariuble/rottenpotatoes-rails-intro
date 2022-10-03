4043f324da8094dbd1d06881474d414744f8dae3
Marius Brogaard Lerstein <mariuslerstein@berkeley.edu>
2022-10-03 00:52:47 +0000

    Added filter feature

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    #['pg']
+    Movie.uniq.pluck(:rating)
+  end
+  def self.with_ratings(ratings)
+    ratings.nil? ? Movies.all : Movie.where(rating: ratings)
+  end
 end
